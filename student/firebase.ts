d8559fc8fb479dd6c87d03c2ad181738bc669e06
haramohan111 <haramohan111@gmail.com>
2025-09-13 17:14:56 +0530

    firebase

--- student/firebase.ts
+++ student/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 import { getStorage } from "firebase/storage";
@@ -23,5 +22,5 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);    //Firestore instance
 export const storage = getStorage(app);
 export const firestore = getFirestore(app); 
-const analytics = getAnalytics(app);
+
 
